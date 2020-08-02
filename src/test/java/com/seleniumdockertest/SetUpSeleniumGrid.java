2906a1d54f90bd883c2e8c0d2b7c6090353d2d0a
Ravindra Deshpande <Ravindra.Deshpande@tomtom.com>
2020-08-02 16:04:06 +0530

    code change

--- src/test/java/com/seleniumdockertest/SetUpSeleniumGrid.java
+++ src/test/java/com/seleniumdockertest/SetUpSeleniumGrid.java
@@ -9,14 +9,14 @@ public class SetUpSeleniumGrid {
 	
 	@BeforeTest
 	public void start() throws IOException, InterruptedException {
-		String[] cmd = { "/bin/sh", "-c", "cd /var; ls -l;docker-compose up" };
+		String[] cmd = { "/bin/sh", "-c", "cd /var/lib/jenkins/workspace/Git_Project_Checkout_Job; ls -l;docker-compose up" };
 		Runtime.getRuntime().exec(cmd);
 	     Thread.sleep(15000);
 	}
 	
 	@AfterTest
 	public void stop() throws IOException, InterruptedException {
-		String[] cmd = { "/bin/sh", "-c", "cd /var; ls -l;docker-compose down" };
+		String[] cmd = { "/bin/sh", "-c", "cd /var/lib/jenkins/workspace/Git_Project_Checkout_Job; ls -l;docker-compose down" };
 		Runtime.getRuntime().exec(cmd);
 	     Thread.sleep(15000);
 	}
