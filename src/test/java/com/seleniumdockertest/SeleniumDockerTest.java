15f5e5d82419d12cf01eaea4438bee1b9e53fddc
Ravindra Deshpande <Ravindra.Deshpande@tomtom.com>
2020-08-02 17:05:39 +0530

    code refactor

--- src/test/java/com/seleniumdockertest/SeleniumDockerTest.java
+++ src/test/java/com/seleniumdockertest/SeleniumDockerTest.java
@@ -78,8 +78,10 @@ public class SeleniumDockerTest {
 	}
 	
 	
-	 @AfterTest public void stop() throws IOException, InterruptedException {
+	 @AfterTest 
+	 public void stop() throws IOException, InterruptedException {
 	 String[] cmd = { "/bin/sh", "-c","cd /var/lib/jenkins/workspace/Git_Project_Checkout_Job; ls -l;docker-compose down"}; 
-	 Process p=Runtime.getRuntime().exec(cmd); p.waitFor(15, TimeUnit.SECONDS);
+	 Process p=Runtime.getRuntime().exec(cmd); 
+	 p.waitFor(15, TimeUnit.SECONDS);
 	  }
 }
