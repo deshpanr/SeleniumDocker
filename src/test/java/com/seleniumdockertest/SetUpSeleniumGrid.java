aeadb13beb62007570e5ada3befa147d547e1a35
Ravindra Deshpande <Ravindra.Deshpande@tomtom.com>
2020-08-02 12:18:54 +0530

    Added TestClass to start and stop docker container and modified testng.xml file.

---
+++ src/test/java/com/seleniumdockertest/SetUpSeleniumGrid.java
@@ -0,0 +1,23 @@
+package com.seleniumdockertest;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class SetUpSeleniumGrid {
+	
+	@BeforeTest
+	public void start() throws IOException, InterruptedException {
+		
+		Runtime.getRuntime().exec("cmd /c start start_selenium_grid_container.sh");
+	     Thread.sleep(15000);
+	}
+	
+	@AfterTest
+	public void stop() throws IOException, InterruptedException {
+		Runtime.getRuntime().exec("cmd /c stop_selenium_grid_container.sh");
+	     Thread.sleep(15000);
+	}
+
+}
